a9295a125e9b87561d5758d658d8f3376ce83809
Jhonata Tirloni <jhonata_tirloni@sicredi.com.br>
2023-10-31 15:41:01 -0300

    API jira esqueletada

--- project/main.py
+++ project/main.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template, redirect, session, url_for
 # import os
 import openai
-# import json
-# from atlassian import jira
+import json
+from atlassian import Jira
 
 
 main = Blueprint('main', __name__)
@@ -15,7 +15,41 @@ def index():
 
 @main.route('/create_jira_task')
 def create_jira_task():
-    session.get('resp_gpt')
+    res_gpt = session.get('resp_gpt')
+    data = json.loads(res_gpt["choices"][0]["message"]["content"])
+    titulo = data["Título"]  # incluir tratamento do retorno para pegar titulo
+    tema = data["Tema"]  # incluir tratamento do retorno para pegar tema
+    descricao = data["Descrição"]  # incluir tratamento do retorno desc.
+    fields = {
+        "project": {"id": 31858},
+        "summary": f"[{tema}]{titulo}",
+        "description": f"{descricao}",
+        "issuetype": {"name": "História Avulsa"},
+        "labels": [f"{tema}"],
+        "customfield_18200": [{"key": "CMDB-1233"}],
+        "customfield_12200": {
+            "self": "https://teams.sicredi.io/rest/api/2/customFieldOption/17702",
+            "value": "Ambos",
+            "id": "17702",
+            "disabled": False,
+        },
+        "customfield_16900": {
+            "self": "https://teams.sicredi.io/rest/api/2/customFieldOption/27201",
+            "value": "Novo Core",
+            "id": "27201",
+            "disabled": False,
+        },
+    }
+
+    jira = Jira(
+        url="https://teams.sicredi.io/",
+        username="",
+        password=""
+    )
+
+    response = jira.issue_create(fields)
+
+    return print(f"task criada com o código {response['key']}")
 
 
 @main.route('/gerar_descricao')
